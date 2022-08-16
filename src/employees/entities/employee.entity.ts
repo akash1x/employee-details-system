6296aead8802d34c867c42d64bf4bb09c3271007
akash1x <akash1x25@gmail.com>
2022-08-16 21:36:15 +0530

    add: employees schema

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,1 +1,44 @@
-export class Employee {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type EmployeeDocument = Employee & Document;
+@Schema()
+class Address {
+  @Prop({ type: String })
+  city: string;
+
+  @Prop({ type: Number })
+  zipCode: number;
+
+  @Prop({ type: String })
+  line1: string;
+
+  @Prop({ type: String })
+  line2?: string;
+}
+
+@Schema()
+export class Employee {
+  @Prop()
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ unique: true })
+  phoneNumber: number;
+
+  @Prop()
+  dateOfBirth: Date;
+
+  @Prop()
+  dateOfEmployement: Date;
+
+  @Prop({ default: false })
+  isDeleted: boolean;
+
+  @Prop({ type: Address })
+  address: Address;
+}
+
+export const EmployeeSchema = SchemaFactory.createForClass(Employee);
